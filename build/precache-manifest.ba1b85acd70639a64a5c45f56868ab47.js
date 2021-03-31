15a77877c36b4232f85870c49714aed4ddfe24b1
bthbandit <bthbandit@gmail.com>
2021-03-31 19:20:30 -0400

    Okay we're committing the build folder, watch out

---
+++ build/precache-manifest.ba1b85acd70639a64a5c45f56868ab47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbd7f9aa8fcce5db6cb50038e9909c7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2953a9a97ea400fb7640",
+    "url": "./static/js/2.a842d044.chunk.js"
+  },
+  {
+    "revision": "f0256323ad9ca968d5ddd5a8ff4b4a4a",
+    "url": "./static/js/2.a842d044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b64bd2b523997ae6b1",
+    "url": "./static/js/main.07360a7d.chunk.js"
+  },
+  {
+    "revision": "1802334ffd0f54caa6ba",
+    "url": "./static/js/runtime-main.c62b29ea.js"
+  }
+]);
