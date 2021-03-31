e5a4424d7008cef035847c1f079c44595a6c9a3e
bthbandit <bthbandit@gmail.com>
2021-03-31 19:04:55 -0400

    Put everything in main plz

---
+++ build/precache-manifest.062d2a09d77da2fe8129a7465a00d5b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3e854c1c54e87192f92474126e2ba9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8f2c464884a7b7a94f2",
+    "url": "./static/js/2.89911f78.chunk.js"
+  },
+  {
+    "revision": "90d3f0df3551e8c04c8113cb7597324f",
+    "url": "./static/js/2.89911f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c590b16dfdb4ae51041",
+    "url": "./static/js/main.e127e342.chunk.js"
+  },
+  {
+    "revision": "1802334ffd0f54caa6ba",
+    "url": "./static/js/runtime-main.c62b29ea.js"
+  }
+]);
